768835d8f96a0a21483e9033f95395a23da2058d
Benah0987 <88454033+Benah0987@users.noreply.github.com>
2023-03-20 14:13:31 +0300

    Update index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+function shout(greeting) {
+    return greeting .toUpperCase();
+    
+}
+const result = shout("helllo");
+
+console.log(result);
+
+//whisper function
+function whisper(slow) {
+    return slow.toLowerCase();
+     
+ }
+ const talk = whisper("HELLO")
+ console.log(talk);
+
+s
